3fffcfd2cd49b5e686b502af2b89ff2d40f8fb92
Eddy Xiao <bewantbe@gmail.com>
2024-04-08 02:13:14 +0800

    Add direct solver.

--- gps_eqn.py
+++ gps_eqn.py
@@ -49,16 +49,51 @@ def gen_sound_data_5p():
     ct = np.zeros(m)         # relative time delay of arrival (unit: s)
 
     # exact positions and time delays
+    p[0] = _a([0.0, 0, -0.05])
+    ct[0] = np.linalg.norm(r_true - p[0])
+    p[1] = _a([0.1, 0, 0])
+    ct[1] = np.linalg.norm(r_true - p[1]) - ct[0]
+    p[2] = _a([0, 0.12, 0])
+    ct[2] = np.linalg.norm(r_true - p[2]) - ct[0]
+    p[3] = _a([-0.15, 0, 0])
+    ct[3] = np.linalg.norm(r_true - p[3]) - ct[0]
+    p[4] = _a([0, -0.11, 0])
+    ct[4] = np.linalg.norm(r_true - p[4]) - ct[0]
+
+    ct0 = ct[0]
+    ct[0] = ct[0] - ct0
+
+    # noise level
+    err_pos = 0* 0.2e-3                 # default 0.2mm
+    sample_rate = 250e3              # default 250kHz
+    err_ct = 0* c * 2.0 / sample_rate   # default 2 samples
+
+    # add measurement noise
+    for j in range(m):
+        p[j]  = p[j]  + err_pos * np.random.randn(3)
+        ct[j] = ct[j] + err_ct  * np.random.randn()
+
+    return p, ct, r_true, ct0
+
+def gen_gps_data_d4():
+    # receiver position (unit: m)
+    r_true = np.array([0.1, 0.2, -0.5])
+    c = 299792458.0
+
+    p = np.zeros((4, 3))     # satellite positions            (unit: m)
+    ct = np.zeros(4)         # relative time delay of arrival (unit: s)
+
+    height = 20180e3 + 6370e3
+
+    # exact positions and time delays
     p[0] = _a([0, 0, 0])
     ct[0] = np.linalg.norm(r_true - p[0])
     p[1] = _a([0.1, 0, 0])
     ct[1] = np.linalg.norm(r_true - p[1]) - ct[0]
-    p[2] = _a([0, 0.1, 0])
+    p[2] = _a([0, 0.10001, 0])
     ct[2] = np.linalg.norm(r_true - p[2]) - ct[0]
-    p[3] = _a([-0.1, 0, 0])
+    p[3] = _a([-0.1001, 0, 0])
     ct[3] = np.linalg.norm(r_true - p[3]) - ct[0]
-    p[4] = _a([0, -0.1, 0])
-    ct[4] = np.linalg.norm(r_true - p[4]) - ct[0]
 
     ct0 = ct[0]
     ct[0] = ct[0] - ct0
@@ -128,6 +163,44 @@ def NewtonIterGPS(p, ct, r_n, ct0_n):
     q = NewtonIter(F, dF, q0)
     return q[:3], q[3]
 
+def DirectGPSSolver(p, ct):
+    """
+    Ref. An Algebraic Solution of the GPS Equations
+    """
+    m = len(ct)
+    A = jnp.hstack([p, ct[:, np.newaxis]])
+    i0 = jnp.ones(m)
+    r_vec = _ja([
+        (jnp.dot(p[j], p[j]) - ct[j]**2) / 2
+        for j in range(m)
+    ])
+    if 0:
+        B = jnp.dot(jnp.linalg.inv(jnp.dot(A.T, A)), A.T)
+        u_vec = B @ i0
+        v_vec = B @ r_vec
+    else:
+        # hope for better numerical stability
+        # But still, for rank-deficient A, the solution is usuall incorrect
+        u_vec = jnp.linalg.lstsq(A, i0, rcond=None)[0].flatten()
+        v_vec = jnp.linalg.lstsq(A, r_vec, rcond=None)[0].flatten()
+    E = jnp.dot(u_vec[:3], u_vec[:3]) - u_vec[3] * u_vec[3]
+    F = jnp.dot(u_vec[:3], v_vec[:3]) - u_vec[3] * v_vec[3] - 1
+    G = jnp.dot(v_vec[:3], v_vec[:3]) - v_vec[3] * v_vec[3]
+    lambdas = jnp.roots(_ja([E, 2*F, G]), strip_zeros = False)
+    y_all = u_vec[:, jnp.newaxis] * lambdas[jnp.newaxis, :] + v_vec[:, jnp.newaxis]
+
+    # pick the real solution
+    y1 = jnp.real(y_all[:, 0])
+    y2 = jnp.real(y_all[:, 1])
+    l1 = jnp.linalg.norm(GPS_F(p, ct, y1[:3], y1[3]))
+    l2 = jnp.linalg.norm(GPS_F(p, ct, y2[:3], y2[3]))
+    if l1 < l2:
+        y = y1
+    else:
+        y = y2
+
+    return y[:3], y[3]
+
 def GetK(r, ct0, p, ct, err_pos, err_ct):
     m = len(ct)
     K = jnp.array([
@@ -174,3 +247,8 @@ if __name__ == '__main__':
     print('Solution:')
     print('r_n', r_n)
     print('ct0_n', ct0_n)
+
+    print('Direct solver:')
+    r_direct, ct0_direct = DirectGPSSolver(p, ct)
+    print('r_direct', r_direct)
+    print('ct0_direct', ct0_direct)
